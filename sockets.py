1e7e3cf98814041f72c84aea7ad3f07fb3dd7258
Anders Johnson <anders.johnson.email@gmail.com>
2020-03-19 17:13:04 -0600

    Licensing, credit to my previous assignment

--- sockets.py
+++ sockets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright (c) 2013-2014 Abram Hindle, Anders Johnson
+# Copyright (c) 2013-2020 Abram Hindle, Anders Johnson
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
